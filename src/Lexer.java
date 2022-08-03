762e0e32c8d4403913f455c78c8404e9dad7dead
Henning Fischel <hefischel@gmail.com>
2022-08-03 15:49:36 -0400

    Update Lexer.java
    Fixed multi-token operations (e.g. >=)
    Added new characters to split tokens on
    Reformatted

--- src/Lexer.java
+++ src/Lexer.java
@@ -5,65 +5,67 @@ import java.util.*;
 
 public class Lexer {
 
-    public static Map<String,String > OPERATION_STRINGS = Map.ofEntries(
-            Map.entry("var","VAR_DEC"),
-            Map.entry("func","FUNC_DEC"),
-            Map.entry("\\+","ADD"),
+    public static Map<String, String> OPERATION_STRINGS = Map.ofEntries(
+            Map.entry("var", "VAR_DEC"),
+            Map.entry("func", "FUNC_DEC"),
+            Map.entry("\\+", "ADD"),
             Map.entry("-", "SUB"),
-            Map.entry("\\*","MUL"),
-            Map.entry("\\/","DIV"),
-            Map.entry(">","GT"),
-            Map.entry(">=","GEQ"),
-            Map.entry("<","LT"),
-            Map.entry("<=","LEQ"),
-            Map.entry("==","EQ"),
-            Map.entry("!","NOT"),
-            Map.entry("!=","NEQ"),
-            Map.entry("\\|\\|","OR"),
-            Map.entry("\\&\\&","AND"),
-            Map.entry("\\(","OPEN_PAREN"),
-            Map.entry("\\)","CLOSE_PAREN"),
-            Map.entry("\\{","OPEN_BRACE"),
-            Map.entry("\\}","CLOSE_BRACE"),
-            Map.entry("if","IF"),
-            Map.entry("else","ELSE"),
-            Map.entry("while","WHILE"),
-            Map.entry("for","FOR"),
-            Map.entry("[0-9]","CONST"),
-            Map.entry("^[a-zA-Z][a-zA-Z0-9]*$","REF"),
-            Map.entry(";","SEMI"),
-            Map.entry("print","PRINT"),
-            Map.entry("=","ASSIGN"),
-            Map.entry("return","RET"),
-            Map.entry(",","COMMA")
+            Map.entry("\\*", "MUL"),
+            Map.entry("\\/", "DIV"),
+            Map.entry(">", "GT"),
+            Map.entry(">=", "GEQ"),
+            Map.entry("<", "LT"),
+            Map.entry("<=", "LEQ"),
+            Map.entry("==", "EQ"),
+            Map.entry("!", "NOT"),
+            Map.entry("!=", "NEQ"),
+            Map.entry("\\|\\|", "OR"),
+            Map.entry("\\&\\&", "AND"),
+            Map.entry("\\(", "OPEN_PAREN"),
+            Map.entry("\\)", "CLOSE_PAREN"),
+            Map.entry("\\{", "OPEN_BRACE"),
+            Map.entry("\\}", "CLOSE_BRACE"),
+            Map.entry("if", "IF"),
+            Map.entry("else", "ELSE"),
+            Map.entry("while", "WHILE"),
+            Map.entry("for", "FOR"),
+            Map.entry("[0-9]", "CONST"),
+            Map.entry("^[a-zA-Z][a-zA-Z0-9]*$", "REF"),
+            Map.entry(";", "SEMI"),
+            Map.entry("print", "PRINT"),
+            Map.entry("=", "ASSIGN"),
+            Map.entry("return", "RET"),
+            Map.entry(",", "COMMA"),
+            Map.entry("%", "MOD")
     );
     ArrayList<String> rawTokens;
     ArrayList<ArrayList<String>> tokensWithTypes;
     String splitRegex;
-    public Lexer(){
+
+    public Lexer() {
         rawTokens = new ArrayList<>();
         tokensWithTypes = new ArrayList<>();
         ArrayList<String> extraSplitTokens = new ArrayList<>(Arrays.asList("\\(", "\\)", "\\[", "\\]", "\\{", "\\}",
-                "<", ">", "<=", ">=", "\\+", "-","\\/","\\*","=","==","!=",";","\\|\\|","\\&\\&"));
+                "<", ">", "<=", ">=", "\\+", "-", "\\/", "\\*", "=", "==", "!=", ";", "\\|\\|", "\\&\\&",","));
         splitRegex = "\\t|\\n| ";
-        for(String s : extraSplitTokens) splitRegex = splitRegex.concat("|((?="+s+")|(?<="+s+"))");
+        for (String s : extraSplitTokens) splitRegex = splitRegex.concat("|((?=" + s + ")|(?<=" + s + "))");
     }
 
     private void readFromFile(String infile) {
-        try{
+        try {
             Scanner in = new Scanner(new File(infile));
             boolean multiLineComment = false;
-            while(in.hasNextLine()){
+            while (in.hasNextLine()) {
                 String line = in.nextLine();
-                if(line.contains("/*")) multiLineComment=true;
-                if(line.contains("*/")){
-                    multiLineComment=false;
-                    while(line.contains("*/")){
-                        line = line.replace("*/","");
+                if (line.contains("/*")) multiLineComment = true;
+                if (line.contains("*/")) {
+                    multiLineComment = false;
+                    while (line.contains("*/")) {
+                        line = line.replace("*/", "");
                     }
                 }
-                if(multiLineComment) continue;
-                if(line.contains("#")) continue;
+                if (multiLineComment) continue;
+                if (line.contains("#")) continue;
                 //split removing whitespace and around parens
                 ArrayList<String> lineToks = new ArrayList<>(Arrays.asList(line.split(splitRegex)));
                 lineToks.removeIf(String::isEmpty);
@@ -76,28 +78,33 @@ public class Lexer {
 
     public void tokenize(String inPath) throws UnexpectedTokenException {
         readFromFile(inPath);
-        Set<String> keys =OPERATION_STRINGS.keySet();
+        Set<String> keys = OPERATION_STRINGS.keySet();
         String refRegex = "^[a-zA-Z][a-zA-Z0-9]*$";
-        for (String s : rawTokens) {
+        for (int i=0; i<rawTokens.size();i++) {
+            String s = rawTokens.get(i);
             boolean matched = false;
-            for (String regex : keys){
+            if(s.matches("<|>|=") && rawTokens.get(i+1).equals("=")){
+                s = s+"=";
+                i++;
+            }
+            for (String regex : keys) {
                 //make sure if/while/... statements don't match with variable and function references
-                if(OPERATION_STRINGS.get(regex).equals("REF")) continue;
-                else if(s.matches(regex)) {
+                if (OPERATION_STRINGS.get(regex).equals("REF")) continue;
+                else if (s.matches(regex)) {
                     tokensWithTypes.add(new ArrayList<>(Arrays.asList(s, OPERATION_STRINGS.get(regex))));
                     matched = true;
                     break;
                 }
             }
-            if(!matched) {
-                if(s.matches(refRegex)) {
+            if (!matched) {
+                if (s.matches(refRegex)) {
                     tokensWithTypes.add(new ArrayList<>(Arrays.asList(s, OPERATION_STRINGS.get(refRegex))));
                 } else {
                     throw new UnexpectedTokenException(s);
                 }
             }
         }
-        writeLexed(inPath.substring(0,inPath.lastIndexOf('.'))+".vlex");
+        writeLexed(inPath.substring(0, inPath.lastIndexOf('.')) + ".vlex");
     }
 
     private void writeLexed(String outfile) {
@@ -105,27 +112,27 @@ public class Lexer {
             File outFile = new File(outfile);
             outFile.delete();
             outFile.createNewFile();
-        } catch (Exception e){
+        } catch (Exception e) {
             System.err.println(e);
         }
         try {
             FileWriter writer = new FileWriter(outfile);
-            for(ArrayList<String> tokenAndType: tokensWithTypes){
-                writer.write(tokenAndType.get(1)+" "+tokenAndType.get(0));
+            for (ArrayList<String> tokenAndType : tokensWithTypes) {
+                writer.write(tokenAndType.get(1) + " " + tokenAndType.get(0));
                 writer.write('\n');
             }
 
             writer.close();
-        } catch (Exception e){
+        } catch (Exception e) {
             System.err.println(e);
         }
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Lexer l = new Lexer();
         try {
             l.tokenize(args[0]);
-        } catch (UnexpectedTokenException e){
+        } catch (UnexpectedTokenException e) {
             System.err.println(e);
         }
     }

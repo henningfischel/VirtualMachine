b94f9ccf62365e28905f480264d6793bdb8b526e
Henning Fischel <hefischel@gmail.com>
2022-08-03 15:47:27 -0400

    reformating

--- src/Compiler.java
+++ src/Compiler.java
@@ -2,7 +2,7 @@
  * The Compiler Class converts a file of english text instructions for Machine and converts them into a file of integers
  * that a Machine object can execute.
  *
- * @author  Henning Fischel
+ * @author Henning Fischel
  */
 
 import java.io.File;
@@ -18,48 +18,50 @@ import static java.util.Map.entry;
 
 public class Compiler {
 
-    public static final Map<String,Integer> INSTRUCTIONS = Map.ofEntries(
-         entry("ADD", 1),        // int add
-         entry("SUB", 2),         // int sub
-         entry("MUL", 3),        // int mul
-         entry("DIV", 4),          // int divide
-         entry("LT", 5),         // int less than
-         entry("GT", 6),         // int grater than
-         entry("LEQ", 7),        // int less than eq
-         entry("GEQ", 8),        // int greater than eq
-         entry("EQ", 9),         // int equal
-         entry("AND", 10),        // boolean and
-         entry("OR", 11),        // boolean or
-         entry("NOT", 12),       // boolean not
-         entry("JMP", 13),        // branch
-         entry("JMPT", 14),       // branch if true
-         entry("JMPF", 15),       // branch if false
-         entry("CONST", 16),      // push constant integer
-         entry("LOAD", 17),      // load from local
-         entry("GLOAD", 18),     // load from global
-         entry("STORE", 19),     // store in local
-         entry("GSTORE", 20),    // store in global memory
-         entry("PRINT", 21),     // print value on top of the stack
-         entry("POP", 22),       // throw away top of the stack
-         entry("HALT", 23),      // stop program
-         entry("CALL", 24),      // call procedure
-         entry("RET", 25)       // return from procedure
+    public static final Map<String, Integer> INSTRUCTIONS = Map.ofEntries(
+            entry("ADD", 1),        // int add
+            entry("SUB", 2),         // int sub
+            entry("MUL", 3),        // int mul
+            entry("DIV", 4),          // int divide
+            entry("LT", 5),         // int less than
+            entry("GT", 6),         // int grater than
+            entry("LEQ", 7),        // int less than eq
+            entry("GEQ", 8),        // int greater than eq
+            entry("EQ", 9),         // int equal
+            entry("AND", 10),        // boolean and
+            entry("OR", 11),        // boolean or
+            entry("NOT", 12),       // boolean not
+            entry("JMP", 13),        // branch
+            entry("JMPT", 14),       // branch if true
+            entry("JMPF", 15),       // branch if false
+            entry("CONST", 16),      // push constant integer
+            entry("LOAD", 17),      // load from local
+            entry("GLOAD", 18),     // load from global
+            entry("STORE", 19),     // store in local
+            entry("GSTORE", 20),    // store in global memory
+            entry("PRINT", 21),     // print value on top of the stack
+            entry("POP", 22),       // throw away top of the stack
+            entry("HALT", 23),      // stop program
+            entry("CALL", 24),      // call procedure
+            entry("RET", 25),       // return from procedure
+            entry("NEG", 26),
+            entry("MOD", 27)
     );
 
-    public static final Map<Integer,String> INT_TO_INSTRUCTION = //an inverse map of INSTRUCTIONS
+    public static final Map<Integer, String> INT_TO_INSTRUCTION = //an inverse map of INSTRUCTIONS
             INSTRUCTIONS.entrySet().stream().collect(Collectors.toMap(Map.Entry::getValue, Map.Entry::getKey));
 
 
-    public Compiler(){
+    public Compiler() {
     }
 
     private static ArrayList<String> readFromFile(String infile) {
         ArrayList<String> tokens = new ArrayList<>();
-        try{
+        try {
             Scanner in = new Scanner(new File(infile));
-            while(in.hasNextLine()){
+            while (in.hasNextLine()) {
                 String line = in.nextLine();
-                if(!line.contains("#"))
+                if (!line.contains("#"))
                     tokens.addAll(Arrays.asList(line.split("\\s")));
             }
         } catch (FileNotFoundException e) {
@@ -72,26 +74,26 @@ public class Compiler {
         int[] ints = new int[tokens.size()];
         int skippedTokens = 0;
         int prgrmPointer = 0;
-        for(String token : tokens) {
-            if(token.isEmpty()){
+        for (String token : tokens) {
+            if (token.isEmpty()) {
                 skippedTokens++;
                 continue;
             }
-            if(INSTRUCTIONS.containsKey(token)){
-                ints[prgrmPointer]= INSTRUCTIONS.get(token);
+            if (INSTRUCTIONS.containsKey(token)) {
+                ints[prgrmPointer] = INSTRUCTIONS.get(token);
                 prgrmPointer++;
             } else {
                 try {
                     int tmp = Integer.parseInt(token);
                     ints[prgrmPointer] = tmp;
                     prgrmPointer++;
-                } catch (NumberFormatException e){
+                } catch (NumberFormatException e) {
                     System.err.println(e);
                     skippedTokens++;
                 }
             }
         }
-        return Arrays.copyOfRange(ints,0,ints.length-skippedTokens);
+        return Arrays.copyOfRange(ints, 0, ints.length - skippedTokens);
     }
 
     private static void writeProgram(int[] program, String outfile) {
@@ -99,31 +101,31 @@ public class Compiler {
             File outFile = new File(outfile);
             outFile.delete();
             outFile.createNewFile();
-        } catch (Exception e){
+        } catch (Exception e) {
             System.err.println(e);
         }
         try {
             FileWriter writer = new FileWriter(outfile);
-            for(int instruction : program){
+            for (int instruction : program) {
                 writer.write(Integer.toString(instruction));
                 writer.write(' ');
             }
 
             writer.close();
-        } catch (Exception e){
+        } catch (Exception e) {
             System.err.println(e);
         }
     }
 
-    public static void compile(String infile,String outfile){
+    public static void compile(String infile, String outfile) {
         ArrayList<String> tokens = readFromFile(infile);
         int[] program = tokensToInts(tokens);
-        writeProgram(program,outfile);
+        writeProgram(program, outfile);
     }
 
     public static void main(String[] args) {
-        if(args.length==2){
-            compile(args[0],args[1]);
+        if (args.length == 2) {
+            compile(args[0], args[1]);
         } else {
             System.err.println("Not enough arguments. Please use arguments: input_file output_file");
         }

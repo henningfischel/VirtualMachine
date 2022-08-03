119896e39836305bf32f73d36492806c45d2f5b0
Henning Fischel <hefischel@gmail.com>
2022-08-03 15:53:44 -0400

    Update Machine.java
    Added negation and modulo operations
    Reformatted whitespace

--- src/Machine.java
+++ src/Machine.java
@@ -1,7 +1,7 @@
 /**
  * The Machine Class is a virtual machine that executes a program
  *
- * @author  Henning Fischel
+ * @author Henning Fischel
  */
 
 import java.util.ArrayList;
@@ -12,16 +12,16 @@ import java.util.Scanner;
 
 public class Machine {
     private int[] programMemory, stack, globalMem;
-    private int pc,sp, fp;
-    private  int a,b;
+    private int pc, sp, fp;
+    private int a, b;
     private boolean done, debug = false;
 
     // the instruction set for the machine
     public final static int
             ADD = 1,        // int add
-            SUB= 2,         // int sub
+            SUB = 2,         // int sub
             MUL = 3,        // int mul
-            DIV=4,          // int divide
+            DIV = 4,          // int divide
             LT = 5,         // int less than
             GT = 6,         // int grater than
             LEQ = 7,        // int less than eq
@@ -42,9 +42,11 @@ public class Machine {
             POP = 22,       // throw away top of the stack
             HALT = 23,      // stop program
             CALL = 24,      // call procedure
-            RET = 25;       // return from procedure
+            RET = 25,       // return from procedure
+            NEG = 26,       // negate an int
+            MOD = 27;       // modulo
 
-    public Machine(int[] program){
+    public Machine(int[] program) {
         sp = -1;
         pc = 0;
         fp = 0;
@@ -54,7 +56,7 @@ public class Machine {
         globalMem = new int[300];
     }
 
-    public Machine(String filepath){
+    public Machine(String filepath) {
         sp = -1;
         pc = 0;
         done = false;
@@ -63,14 +65,15 @@ public class Machine {
         globalMem = new int[300];
     }
 
-    public Machine(){
+    public Machine() {
         sp = -1;
         pc = 0;
         done = false;
         stack = new int[100];
         globalMem = new int[300];
     }
-    public Machine(boolean debug){
+
+    public Machine(boolean debug) {
         sp = -1;
         pc = 0;
         this.debug = debug;
@@ -79,28 +82,28 @@ public class Machine {
         globalMem = new int[300];
     }
 
-    public void load(String path){
-        try{
+    public void load(String path) {
+        try {
             ArrayList<Integer> tmp = new ArrayList<Integer>();
             Scanner in = new Scanner(new File(path));
-            while(in.hasNextInt()){
+            while (in.hasNextInt()) {
                 tmp.add(in.nextInt());
             }
-            programMemory=tmp.stream().mapToInt(Integer::intValue).toArray();
+            programMemory = tmp.stream().mapToInt(Integer::intValue).toArray();
         } catch (FileNotFoundException e) {
             System.err.println(e);
-            programMemory = new int[] {HALT};
+            programMemory = new int[]{HALT};
         }
     }
 
-    public void load(int[] program){
+    public void load(int[] program) {
         programMemory = program.clone();
     }
 
-    public void run(){
+    public void run() {
         done = false;
-        while(!done){
-            if(debug) System.out.print(Compiler.INT_TO_INSTRUCTION.get(programMemory[pc]));
+        while (!done) {
+            if (debug) System.out.print(Compiler.INT_TO_INSTRUCTION.get(programMemory[pc]));
             switch (programMemory[pc]) {
                 case ADD -> {
                     b = pop();
@@ -125,55 +128,55 @@ public class Machine {
                 case LT -> {
                     b = pop();
                     a = pop();
-                    push(a<b? 1:0);
+                    push(a < b ? 1 : 0);
                 }
                 case GT -> {
                     b = pop();
                     a = pop();
-                    push(a>b? 1:0);
+                    push(a > b ? 1 : 0);
                 }
                 case LEQ -> {
                     b = pop();
                     a = pop();
-                    push(a<=b? 1:0);
+                    push(a <= b ? 1 : 0);
                 }
                 case GEQ -> {
                     b = pop();
                     a = pop();
-                    push(a>=b? 1:0);
+                    push(a >= b ? 1 : 0);
                 }
                 case EQ -> {
                     b = pop();
                     a = pop();
-                    push(a==b? 1:0);
+                    push(a == b ? 1 : 0);
                 }
                 case AND -> {
                     b = pop();
                     a = pop();
-                    push(a==1&&b==1? 1:0);
+                    push(a == 1 && b == 1 ? 1 : 0);
                 }
                 case OR -> {
                     b = pop();
                     a = pop();
-                    push(a==1||b==1? 1:0);
+                    push(a == 1 || b == 1 ? 1 : 0);
                 }
                 case NOT -> {
                     a = pop();
-                    push(a==1? 0: a==0? 1: a);
+                    push(a == 1 ? 0 : a == 0 ? 1 : a);
                 }
                 case JMP -> {   // pop the address off the stack and change the program counter to it
                     a = pop();
-                    pc = a-1;
+                    pc = a - 1;
                 }
                 case JMPT -> {
                     b = pop();
                     a = pop();
-                    pc = a==1? b-1:pc;
+                    pc = a == 1 ? b - 1 : pc;
                 }
                 case JMPF -> {
                     b = pop();
                     a = pop();
-                    pc = a==0? b-1:pc;
+                    pc = a == 0 ? b - 1 : pc;
                 }
                 case CONST -> {     // push the number following the command in the program memory to the stack
                     pc += 1;
@@ -181,7 +184,7 @@ public class Machine {
                 }
                 case LOAD -> {
                     a = pop(); //get the address
-                    b = a<0? fp+a-2 : fp+a+1;   //avoid the saved data
+                    b = a < 0 ? fp + a - 2 : fp + a + 1;   //avoid the saved data
                     push(stack[b]);
                 }
                 case GLOAD -> {
@@ -189,12 +192,12 @@ public class Machine {
                     push(globalMem[a]);
                 }
                 case STORE -> {
-                    pc+=1;
+                    pc += 1;
                     a = pop();  //get the value to store
-                    stack[fp-programMemory[pc]+1] = a; //store
+                    stack[fp - programMemory[pc] + 1] = a; //store
                 }
-                case  GSTORE -> {
-                    pc+=1;
+                case GSTORE -> {
+                    pc += 1;
                     a = pop();
                     globalMem[programMemory[pc]] = a;
                 }
@@ -203,13 +206,13 @@ public class Machine {
                 case HALT -> done = true;
                 case CALL -> {
                     //save the state
-                    push(programMemory[pc+2]);  //save number of args
-                    push(pc+3); //save the address of the next command -1 (since the loop will add one)
+                    push(programMemory[pc + 2]);  //save number of args
+                    push(pc + 3); //save the address of the next command -1 (since the loop will add one)
                     push(fp);
 
                     fp = sp; //set the frame pointer to the top of the stack
-                    sp += programMemory[pc+3];   //add space for locals
-                    pc = programMemory[pc+1]-1;   //branch to the code of the function
+                    sp += programMemory[pc + 3];   //add space for locals
+                    pc = programMemory[pc + 1] - 1;   //branch to the code of the function
                 }
                 case RET -> {
                     a = pop();      //the return value
@@ -220,22 +223,31 @@ public class Machine {
                     sp -= b;    //discard the function arguments
                     push(a);        //save the return
                 }
+                case NEG -> {
+                    a = pop();
+                    push(-a);
+                }
+                case MOD -> {
+                    b = pop();
+                    a = pop();
+                    push(a % b);
+                }
             }
 
-            if(debug) System.out.println(" pc"+pc+" sp"+sp+" fp"+fp+" stack"
-                    +Arrays.toString(Arrays.copyOfRange(stack,0,sp+1)));
+            if (debug) System.out.println(" pc" + pc + " sp" + sp + " fp" + fp + " stack"
+                    + Arrays.toString(Arrays.copyOfRange(stack, 0, sp + 1)));
             pc++;
         }
     }
 
-    private void push(int n){
+    private void push(int n) {
         sp++;
         stack[sp] = n;
     }
 
-    private int pop(){
+    private int pop() {
         sp--;
-        return stack[sp+1];
+        return stack[sp + 1];
     }
 
     public static void main(String[] args) {
@@ -243,7 +255,7 @@ public class Machine {
         Machine m = new Machine(debug);
         switch (args[0]) {
             case "test":
-                m.load(new int[] {CONST,2,CONST,1,SUB,PRINT,HALT});
+                m.load(new int[]{CONST, 2, CONST, 1, SUB, PRINT, HALT});
                 m.run();
                 break;
             case "load":
@@ -255,8 +267,8 @@ public class Machine {
                 m.run();
                 break;
             case "runSrc":
-                String fname = args[1].substring(0,args[1].lastIndexOf('.'))+".vcomp";
-                Compiler.compile(args[1],fname);
+                String fname = args[1].substring(0, args[1].lastIndexOf('.')) + ".vcomp";
+                Compiler.compile(args[1], fname);
                 m.load(fname);
                 m.run();
         }
